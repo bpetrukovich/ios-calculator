7183557a237e0c0e4465518cf4bac4f32164884c
Bogdan Petrukovich <viorbrint@gmail.com>
2025-09-12 22:39:55 +0300

    refactor and structure

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { ButtonLayout } from "./button-layout";
+import { CalculatorApp } from "./calculator-app";
 import "./style.css";
+import { ButtonLayout } from "./ui/button-layout";
 
 new ButtonLayout();
+new CalculatorApp();
