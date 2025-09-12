3e62faf22ae157b498abe3e8d248fdfec5650d73
Bogdan Petrukovich <viorbrint@gmail.com>
2025-09-13 00:53:32 +0300

    debug view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,57 @@
 import { CalculatorApp } from "./calculator-app";
+import { eventEmitter } from "./event-emitter";
 import "./style.css";
 import { ButtonLayout } from "./ui/button-layout";
+import { CalculatorButton } from "./ui/calculator-button";
+import { NumberCalculatorButton } from "./ui/number-calculator-button";
+import { THEME } from "./ui/theme";
 
-new ButtonLayout();
-new CalculatorApp();
+eventEmitter.on("changeDisplay", (input) => {
+  document.querySelector("#display").innerText = input;
+});
+// new ButtonLayout();
+const app = new CalculatorApp();
+
+class UI {
+  constructor() {
+    this.numbers = [];
+    this.numbers.push(new NumberCalculatorButton(0, () => app.clickZero()));
+    this.numbers.push(new NumberCalculatorButton(1, () => app.clickOne()));
+    this.numbers.push(new NumberCalculatorButton(2, () => app.clickTwo()));
+    this.numbers.push(new NumberCalculatorButton(3, () => app.clickThree()));
+    this.numbers.push(new NumberCalculatorButton(4, () => app.clickFour()));
+    this.numbers.push(new NumberCalculatorButton(5, () => app.clickFive()));
+    this.numbers.push(new NumberCalculatorButton(6, () => app.clickSix()));
+    this.numbers.push(new NumberCalculatorButton(7, () => app.clickSeven()));
+    this.numbers.push(new NumberCalculatorButton(8, () => app.clickEight()));
+    this.numbers.push(new NumberCalculatorButton(9, () => app.clickNine()));
+
+    this.numbers.forEach((button) => {
+      document.querySelector("#app").appendChild(button.createButton());
+    });
+
+    this.operators = [];
+    this.operators.push(
+      new CalculatorButton("+", THEME.rightButtons, () => app.clickAdd()),
+    );
+    this.operators.push(
+      new CalculatorButton("-", THEME.rightButtons, () => app.clickSubtract()),
+    );
+    this.operators.push(
+      new CalculatorButton("*", THEME.rightButtons, () => app.clickMultiply()),
+    );
+    this.operators.push(
+      new CalculatorButton("/", THEME.rightButtons, () => app.clickDevide()),
+    );
+
+    this.operators.push(
+      new CalculatorButton("=", THEME.rightButtons, () => app.clickEquals()),
+    );
+
+    this.operators.forEach((button) => {
+      document.querySelector("#app").appendChild(button.createButton());
+    });
+  }
+}
+
+new UI();
--- src/ui/number-calculator-button.js
+++ src/ui/number-calculator-button.js
@@ -2,7 +2,7 @@ import { CalculatorButton } from "./calculator-button";
 import { THEME } from "./theme";
 
 export class NumberCalculatorButton extends CalculatorButton {
-  constructor(number) {
-    super(number.toString(), THEME.numbers, null); // TODO: null here
+  constructor(number, handler) {
+    super(number.toString(), THEME.numbers, handler); // TODO: null here
   }
 }
