8cbf89306a759fedeeda6b73001d338c79949259
Bogdan Petrukovich <viorbrint@gmail.com>
2025-09-13 14:09:26 +0300

    remove useless class

--- src/ui/number-calculator-button.js
+++
@@ -1,7 +0,0 @@
-import { CalculatorButton } from "./calculator-button";
-
-export class NumberCalculatorButton extends CalculatorButton {
-  constructor(handler) {
-    super(handler); // TODO: null here
-  }
-}
